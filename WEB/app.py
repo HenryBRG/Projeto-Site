33a26c331bd793b684d3e1978f49e820e37069fe
HenryBRG <144804990+HenryBRG@users.noreply.github.com>
2023-10-27 09:09:07 -0300

    Adicionei imagens
    consegui deixer o carrossel e as imagens na mesma line

--- WEB/app.py
+++ WEB/app.py
@@ -13,4 +13,4 @@ def quemsomos():
     return render_template('quemsomos.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- WEB/venv/Scripts/activate.bat
+++ WEB/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Manhã\Downloads\WEB\venv
+set VIRTUAL_ENV=C:\Users\Manhã\Documents\GitHub\Projeto-Site\WEB\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
