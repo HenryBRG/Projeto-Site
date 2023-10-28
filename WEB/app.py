ded827288f7cb86b28c8bbde6ab5cec6421e33b5
HenryBRG <144804990+HenryBRG@users.noreply.github.com>
2023-10-28 18:26:08 -0300

    Página Home Finalizada e Começo da Página Resultados
    finalizei a página home, faltando apenas adicionar o conteúdo(texto). Além disso comecei a página de resultados, que é a página direcionada da barra de pesquisa. Além disso também, comecei a lista de receitas.

--- WEB/app.py
+++ WEB/app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
+@app.route('/resultados')
+def resultados():
+    return render_template('resultados.html')
 
-@app.route('/quemsomos')
-def quemsomos():
-    return render_template('quemsomos.html')
+@app.route('/pesquisa', methods=['POST'])
+def pesquisa():
+    termo_pesquisa = request.form.get('termo_pesquisa')
+    return redirect(url_for('mostrar_resultados', termo_pesquisa=termo_pesquisa))
+
+@app.route('/resultados/<termo_pesquisa>')
+def mostrar_resultados(termo_pesquisa):
+    return render_template('resultados.html', termo_pesquisa=termo_pesquisa)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- WEB/venv/Scripts/activate.bat
+++ WEB/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Manhã\Documents\GitHub\Projeto-Site\WEB\venv
+set VIRTUAL_ENV=C:\Users\gusta\OneDrive\Documentos\GitHub\Projeto-Site\WEB\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
